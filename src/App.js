49543eadf8d9ea3777932ede2a664b6a674393d2
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-06-09 01:12:31 +0600

    Header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="">
-    <h1>Bismillah</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
+
+
   );
 }
 
